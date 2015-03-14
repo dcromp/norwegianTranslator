cdf5fe02fd72336ae79e29487727381047acd372
dcromp <dcromp@live.co.uk>
2015-03-14 22:34:03 +0100

    Initial Commit

---
+++ norwegianTranslator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  norwegianTranslator
+//
+//  Created by David on 14/03/2015.
+//  Copyright (c) 2015 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
