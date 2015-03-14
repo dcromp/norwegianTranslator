a0fbb79586092da0460da486a36e9935533d8f85
dcromp <dcromp@live.co.uk>
2015-03-15 00:00:40 +0100

    Shell Application
    Outlets and actions, core data unloaded

--- norwegianTranslator/ViewController.swift
+++ norwegianTranslator/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var translationLabel: UILabel!
+    @IBAction func translateButton(sender: AnyObject) {
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
